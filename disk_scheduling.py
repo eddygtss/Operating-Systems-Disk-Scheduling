42a12f9c107216d43de1092d0f31f4a042bd78a0
Eddy <eddyh95@gmail.com>
2021-04-12 16:52:04 -0500

    Completed FCFS implementation.

--- disk_scheduling.py
+++ disk_scheduling.py
@@ -7,10 +7,29 @@ parser.add_argument('--algo', type=str, help='type of algorithm to use. Acceptab
 args = parser.parse_args()
 
 
-def fcfs(req):
+def fcfs(head, req):
+    current = head
+    for position in req:
+        print(str(current) + '->' + position)
+        current = position
+
+
+def scan(head, req):
+    current = head
+
+
+def cscan(head, req):
     print(req)
 
 
 if __name__ == '__main__':
-    request = [line.split(",") for line in args.requests]
-    fcfs(request)
+    request = args.requests.split(',')
+    algorithm = args.algo
+    head_pos = args.head_pos
+
+    if algorithm.lower() == 'fcfs':
+        fcfs(head_pos, request)
+    elif algorithm.lower() == 'scan':
+        scan(head_pos, request)
+    elif algorithm.lower() == 'cscan':
+        cscan(head_pos, request)
